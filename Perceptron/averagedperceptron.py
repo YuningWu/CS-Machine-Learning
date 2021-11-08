b58cfa00a7054086e1cc073b4ed8d2db164cf2a1
YuningWu <89426804+YuningWu@users.noreply.github.com>
2021-11-08 12:37:52 -0700

    Update averagedperceptron.py

--- Perceptron/averagedperceptron.py
+++ Perceptron/averagedperceptron.py
@@ -4,7 +4,7 @@ Averaged perceptron
 import pandas as pd
 import numpy as np
 import os
-os.chdir('/Users/Yuning/Desktop/')
+#os.chdir('/Users/Yuning/Desktop/')
 class Perceptron():
     def __init__(self):
         self.w=None
